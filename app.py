324f2b11303bf266cadb20ad965b1023858aeae3
Xatrucho <marlonenriqueponcevillatoro@gmail.com>
2021-11-03 11:07:29 -0500

    updating file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask; request, jsonify
-from flask_sqlalchemy import flask_sqlalchemy
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 import os
@@ -10,3 +10,28 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "ap
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 CORS(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    author = db.Column(db.String, nullable=False)
+    review = db.Column(db.String(144), nullable=False)
+    genre = db.Column(db.String, nullable=True)
+
+    def __init__(self, title, author, review, genre):
+        self.title = title
+        self.author = author
+        self.review = review
+        self.genre= genre
+        
+class BookSchema(ma.Schema):
+    class Meta:
+        fields: ("id", "title", "author", "review", "genre")
+
+book_schema = BookSchema()
+multiple_book_schema = BookSchema(many=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
