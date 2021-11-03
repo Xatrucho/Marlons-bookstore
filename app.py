41029463aa563601bcc3359337550d393abdc4f6
Xatrucho <marlonenriqueponcevillatoro@gmail.com>
2021-11-03 12:47:22 -0500

    added post route

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False, unique=True)
     author = db.Column(db.String, nullable=False)
-    review = db.Column(db.String(144), nullable=False)
+    review = db.Column(db.String(144), nullable=True)
     genre = db.Column(db.String, nullable=True)
 
     def __init__(self, title, author, review, genre):
@@ -31,6 +31,29 @@ class BookSchema(ma.Schema):
 
 book_schema = BookSchema()
 multiple_book_schema = BookSchema(many=True)
+
+@app.route("/book/add", methods=["POST"])
+def add_book():
+    if request.content_type  != "application/json":
+        return jsonify('Error: Data must be JSON.')
+
+    post_data = request.get_json()
+    title = post_data.get('title')
+    author = post_data.get('author')
+    review = post_data.get('review')
+    genre = post_data.get('genre')
+
+    if title == None:
+         return jsonify("Error: Data must have a 'Title' key.")
+
+    if author == None:
+        return jsonify("Error: Data must have a 'Author' key.")
+
+    new_record = Book(title, author, review, genre)
+    db.session.add(new_record)
+    db.session.commit()
+
+    return jsonify("you got it!")
 
 
 if __name__ == "__main__":
